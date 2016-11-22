5b553dbc9c47881411a9f1350a3226418416dc06
eersinyildiz <eersinyildiz@gmail.com>
2016-11-22 23:15:58 +0300

    HW04 Uploaded

---
+++ HW04/IyteMutableSet.scala
@@ -0,0 +1,59 @@
+class IyteMutableSet {
+  var arraySize = 20
+  val list = new Array[Int](arraySize)
+  var numberOfElement = 0
+  def add(element: Int): Unit = {
+    if (numberOfElement == arraySize)
+      println("Can not add more elements.")
+
+    if (!contains(element)) {
+
+      if (numberOfElement <= arraySize) {
+
+        list(numberOfElement) = element
+
+        numberOfElement += 1
+
+      }
+
+    }
+  }
+ 
+  def contains(element: Int): Boolean = {
+    var exist: Boolean = false
+    var lowerBound = 0;
+    var upperBound = numberOfElement - 1;
+    var curIn = 0
+
+    while (lowerBound <= upperBound) {
+
+      curIn = (upperBound + lowerBound) / 2;
+
+      if (list(curIn) > element) {
+        upperBound = curIn - 1;
+      } else if (list(curIn) < element) {
+        lowerBound = curIn + 1; // its in the upper
+      } else if (list(curIn) == element)
+        return true
+    }
+    return exist
+  }
+  override def toString: String = {
+    var result = ""
+
+    if (numberOfElement != 0) {
+
+      for (i <- 0 to numberOfElement - 1) {
+        result += list(i).toString + ", "
+      }
+    }
+    result.dropRight(2)
+  }
+}
+object IyteMutableSet {
+
+  def apply() = {
+    new IyteMutableSet()
+  }
+
+}
